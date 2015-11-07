3330bd065de19a0e65d558a9e7013a491759e912
Bruno Magalhaes <brunoeee@gmail.com>
2015-11-07 13:03:03 -0200

    - General - General Militar - General Recursos - Batedor - Cabo + Soldado

--- A3PIExample/MeuAgentePrincipal.cpp
+++ A3PIExample/MeuAgentePrincipal.cpp
@@ -5,6 +5,14 @@
 #include <BWAPI\UnitType.h>
 
 //blackboard!
+BWAPI::Position base;
+BWAPI::Position base_inimiga;
+
+Unidade* Protoss_Nexus;
+Unidade* Protoss_Gateway;
+
+Unidade* batedor;
+
 bool GameOver = false;
 Unidade* amigoDaVez = NULL;
 //
@@ -41,39 +49,34 @@ void AIConstrutora (Unidade* u){
 	amigoDaVez = NULL;//Bug aqui: amigoDaVez vai ser escolhido de novo antes mesmo do predio terminar...
 }
 
-void AICentroComando (Unidade* u){
-	u->train(BWAPI::UnitTypes::Protoss_Probe);
-	u->train(BWAPI::UnitTypes::Protoss_Zealot);//se for um gateway
-	if((u->supplyTotal() - u->supplyUsed() < 5 || u->minerals() > 449) && amigoDaVez == NULL){
-		//botar no "blackboard" para alguem construir predios de supply
-		std::set<Unidade*> amigos = u->getAllyUnits();
-		for(std::set<Unidade*>::iterator it = amigos.begin(); it != amigos.end(); it++){
-			if((*it)->getType().isWorker()){
-					amigoDaVez = *it;
-					break;
-			}
-		}
-	}//Lembrar que ha varias threads rodando em paralelo. O erro presente neste metodo (qual?) nao resulta em crash do jogo, mas outros poderiam.
+void AIBatedor (Unidade* u){
+
+	int heig = AgentePrincipal::mapHeight();
+    int wid = AgentePrincipal::mapWidth();
+	if(AgentePrincipal::isWalkable(wid, heig)){
+	
+		u->move(BWAPI::Position(wid,heig));
+	
+	}
+	
 }
 
-void AIGuerreiro (Unidade* u){
-	u->attack(*(u->getEnemyUnits().begin())); //ataca uma unidade inimiga aleatoria. Assume que existe uma.
+Unidade* AIGuerreiro (Unidade* caboSoldado[]){
+
+	Unidade* cabo = caboSoldado[0];
+	Unidade* self = caboSoldado[1];
+
+	self->attack(*(self->getEnemyUnits().begin())); //ataca uma unidade inimiga aleatoria. Assume que existe uma.
 	//cuidado com bugs como este. O codigo acima daria crash de null pointer no exato momento que o time inimigo
 	//nao possuisse mais unidades, antes da partida de fato acabar.
+
+	return cabo;
 }
 
 DWORD WINAPI threadAgente(LPVOID param){
 	
-	Unidade *u = (Unidade*) param;
-
-	//exemplos de metodos uteis para construir predios
-	bool x = u->hasPower(3,4,50,60);
-	u->isBuildable(50,50);
-	u->isBuildable(BWAPI::TilePosition(3,5));
-
-	int heig = AgentePrincipal::mapHeight();
-	int wid = AgentePrincipal::mapWidth();
-	//A classe Agente Principal ainda tem o metodo AgentePrincipal bool isWalkable(int x, int y).
+	Unidade* u = (Unidade*) param;
+	Unidade *caboSoldado[2] = {NULL,u}; //caso seja soldado
 
 	while(true){
 		//Se houve algum problema (ex: o jogo foi fechado) ou a unidade estah morta, finalizar a thread
@@ -91,14 +94,109 @@ DWORD WINAPI threadAgente(LPVOID param){
 		//Inserir o codigo de voces a partir daqui//
 		if(u->isIdle()){ //nao ta fazendo nada, fazer algo util
 			if(u == amigoDaVez) AIConstrutora(u);
+			else if(u == batedor) AIBatedor(u);
 			else if(u->getType().isWorker()) AITrabalhador(u);
-			else if(u->getType().canProduce()) AICentroComando(u);
-			else AIGuerreiro(u);
+			else {caboSoldado[0] = AIGuerreiro(caboSoldado);}
 		}
 		else if(u->getType().isWorker() && u == amigoDaVez) AIConstrutora(u); //construir msm q estivesse fazendo algo
 		Sleep(10);//Sempre dormir pelo menos 10ms no final do loop, pois uma iteracao da thread é muito mais rápida do que um turno do bwapi.
 	}
 }
+
+DWORD WINAPI general_militar(LPVOID param){
+
+	while(true){
+		//Se houve algum problema (ex: o jogo foi fechado) ou a unidade estah morta, finalizar a thread
+		if(GameOver) return 0;
+		
+		if(Protoss_Nexus != NULL && Protoss_Nexus->exists() && !Protoss_Nexus->checkNovoTurno()){
+			Sleep(10);
+			continue;
+		}
+		//Inserir o codigo de voces a partir daqui//
+		// Codigo General Militar
+
+
+
+
+
+
+
+
+
+		
+		//Fim Codigo Genaral Militar
+		Sleep(10);//Sempre dormir pelo menos 10ms no final do loop, pois uma iteracao da thread é muito mais rápida do que um turno do bwapi.
+	}
+}
+
+DWORD WINAPI general_recursos(LPVOID param){
+
+	while(true){
+		//Se houve algum problema (ex: o jogo foi fechado) ou a unidade estah morta, finalizar a thread
+		if(GameOver) return 0;
+		
+		if(Protoss_Nexus != NULL && Protoss_Nexus->exists() && !Protoss_Nexus->checkNovoTurno()){
+			Sleep(10);
+			continue;
+		}
+		//Inserir o codigo de voces a partir daqui//
+		// Codigo General Recursos
+
+
+
+
+
+
+
+
+
+		
+		//Fim Codigo Genaral recursos
+		Sleep(10);//Sempre dormir pelo menos 10ms no final do loop, pois uma iteracao da thread é muito mais rápida do que um turno do bwapi.
+	}
+}
+
+
+DWORD WINAPI general(LPVOID param){
+
+	while(true){
+		//Se houve algum problema (ex: o jogo foi fechado) ou a unidade estah morta, finalizar a thread
+		if(GameOver) return 0;
+		
+		if(Protoss_Nexus != NULL && Protoss_Nexus->exists() && !Protoss_Nexus->checkNovoTurno()){
+			Sleep(10);
+			continue;
+		}
+		//Inserir o codigo de voces a partir daqui//
+		// Codigo General
+
+
+		//Protoss_Nexus->train(BWAPI::UnitTypes::Protoss_Probe);
+		//Protoss_Gateway->train(BWAPI::UnitTypes::Protoss_Zealot);//se for um gateway
+		if((Protoss_Nexus->supplyTotal() - Protoss_Nexus->supplyUsed() < 5 || Protoss_Nexus->minerals() > 449) && amigoDaVez == NULL){
+			//botar no "blackboard" para alguem construir predios de supply
+			std::set<Unidade*> amigos = Protoss_Nexus->getAllyUnits();
+			for(std::set<Unidade*>::iterator it = amigos.begin(); it != amigos.end(); it++){
+				if((*it)->getType().isWorker()){
+					amigoDaVez = *it;
+					break;
+				}
+			}
+		}//Lembrar que ha varias threads rodando em paralelo. O erro presente neste metodo (qual?) nao resulta em crash do jogo, mas outros poderiam.
+
+
+
+
+
+
+
+		
+		//Fim Codigo Genaral
+		Sleep(10);//Sempre dormir pelo menos 10ms no final do loop, pois uma iteracao da thread é muito mais rápida do que um turno do bwapi.
+	}
+}
+
 
 void MeuAgentePrincipal::InicioDePartida(){
 	//Inicializar estruturas de dados necessarias, ou outras rotinas de inicio do seu programa. Cuidado com concorrencia, 
@@ -116,12 +214,22 @@ void MeuAgentePrincipal::UnidadeCriada(Unidade* unidade){
 	//Uma nova unidade sua foi criada (isto inclui as do inicio da partida). Implemente aqui como quer tratar ela.
 	BWAPI::UnitType tipo = unidade->getType();
 	
-	//Nao desperdicar threads com predios que nao fazem nada
-	if(tipo != BWAPI::UnitTypes::Protoss_Pylon && tipo != BWAPI::UnitTypes::Protoss_Assimilator){
-		CreateThread(NULL,0,threadAgente,(void*)unidade,0,NULL);
+	if(tipo == BWAPI::UnitTypes::Protoss_Nexus){
+		
+		Protoss_Nexus = unidade;
+		CreateThread(NULL,0,general,NULL,0,NULL);
+		CreateThread(NULL,0,general_recursos,NULL,0,NULL);
+		CreateThread(NULL,0,general_militar,NULL,0,NULL);
+
 	}
-	else{
-		//talvez voce queira manter a referencia a estes predios em algum lugar
+	else if(tipo == BWAPI::UnitTypes::Protoss_Gateway){
+	
+		Protoss_Gateway = unidade;
+
+	}
+	//Nao desperdicar threads com predios que nao fazem nada
+	else if(!tipo.canProduce()){
+		CreateThread(NULL,0,threadAgente,(void*)unidade,0,NULL);
 	}
 }
 
